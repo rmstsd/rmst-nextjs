054e35a679285f91990a7279646c8fac2c1f6aaa
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-07-07 19:53:14 +0800

    next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-'use client'
-
+import Button from '@/components/Button'
 import Image from 'next/image'
+import { use } from 'react'
 
 export default function Home() {
-  return <main>rmst next app</main>
+  const data = use(
+    new Promise<number>(resolve => {
+      setTimeout(() => {
+        resolve(123)
+      }, 100)
+    })
+  )
+
+  return (
+    <main>
+      rmst next app
+      <h2>{data}</h2>
+      <Button></Button>
+    </main>
+  )
 }
