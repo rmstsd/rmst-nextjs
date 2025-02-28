042c35bbde6dc11beb5cf65ec3e721accca17119
rmst <rmstsd@qq.com>
2025-02-28 22:19:13 +0800

    re start

---
+++ _legacy/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import Header from '@/components/Header'
+import Sidebar from '@/components/SideBar'
+
+import '../globals.css'
+import '../style.css'
+
+export default async function RootLayout({ children }: React.PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+
+        {/* 
+        <Header />
+        <main className="main">
+          <Sidebar />
+
+          <section className="col note-viewer">{children}</section>
+        </main> */}
+      </body>
+    </html>
+  )
+}
