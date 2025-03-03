9337e74302173b06f9468dc26d7abfdb13a14d2c
rmst <rmstsd@qq.com>
2025-03-03 21:39:06 +0800

    generateStaticParams

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,5 +1,5 @@
 import TimeAgoShow from '@/components/TimeAgo'
-import { findEntityOne } from '@/server'
+import { dataList, findEntityOne } from '@/server'
 import Link from 'next/link'
 
 import type { Metadata, ResolvingMetadata } from 'next'
@@ -20,6 +20,14 @@ export async function generateMetadata({ params, searchParams }: Props, parent: 
   }
 }
 
+export async function generateStaticParams() {
+  console.log('generateStaticParams')
+
+  const ids = dataList.slice(0, 2).map(item => ({ id: item.id }))
+
+  return ids
+}
+
 export default async function Detail({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id
 
@@ -27,9 +35,9 @@ export default async function Detail({ params }: { params: Promise<{ id: string 
   console.log('Detail Page', detail.title)
 
   return (
-    <div>
+    <div style={{ lineHeight: 1.5 }}>
       <Link href="/">回首页</Link>
-      <pre style={{ fontSize: 18, fontFamily: 'Consolas' }}>{JSON.stringify(detail, null, 2)}</pre>
+      <div>标题: {detail.title}</div>
 
       <div>
         创建时间: <TimeAgoShow time={detail.createdOn} />
@@ -37,6 +45,7 @@ export default async function Detail({ params }: { params: Promise<{ id: string 
       <div>
         更新时间: <TimeAgoShow time={detail.modifiedOn} />
       </div>
+      <pre style={{ fontSize: 18, fontFamily: 'Consolas' }}>{JSON.stringify(detail, null, 2)}</pre>
     </div>
   )
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -13,6 +13,8 @@ const headers = {
 export async function findEntityPage() {
   const req = { entityName: 'Article', query: {}, pageNo: 1, pageSize: 10, sort: ['-id'] }
 
+  return dataList
+
   const r = await fetch(`${baseURL}/entity/find/page`, {
     method: 'POST',
     headers,
@@ -24,6 +26,8 @@ export async function findEntityPage() {
 }
 
 export async function findEntityOne(id: string) {
+  return dataList.find(item => item.id === id)
+
   const r = await fetch(`${baseURL}/entity/find/one`, {
     method: 'POST',
     headers,
@@ -32,3 +36,52 @@ export async function findEntityOne(id: string) {
 
   return r.entityValue
 }
+
+export const dataList = [
+  {
+    summary: 'summary 哈哈哈',
+    modifiedOn: 1740990738738,
+    createdBy: '67BBE095E1621E19CBC5D767',
+    author: 'lcl-3',
+    modifiedBy: '67BBE095E1621E19CBC5D767',
+    id: '67C56912E2FD5A1A87B4F46C',
+    title: '哈哈哈',
+    createdOn: 1740990738738,
+    content: 'content 哈哈哈 content 哈哈哈 content 哈哈哈'
+  },
+  {
+    cover: {
+      originalName: '24.png',
+      path: 'upload\\20250362\\67C55833E2FD5A1A87B49DE5.png',
+      size: 35382,
+      md5: null
+    },
+    summary: '东北的冬 刮西北风',
+    modifiedOn: 1740994435066,
+    createdBy: '67BBE095E1621E19CBC5D767',
+    author: 'lcl-2',
+    modifiedBy: '67BBE095E1621E19CBC5D767',
+    id: '67C5584EE2FD5A1A87B49E0A',
+    title: '东北的冬3',
+    createdOn: 1740986446986,
+    version: 1,
+    content: '东北的冬 content 刮西北风，东北的冬 content 刮西北风，东北的冬 content 刮西北风'
+  },
+  {
+    cover: {
+      originalName: 'u=2842321530,644071605&fm=253&app=120&f=JPEG.jpg',
+      path: 'upload\\20250362\\67C557C5E2FD5A1A87B49D4E.jpg',
+      size: 37615,
+      md5: null
+    },
+    summary: '家 summary 人 summary 们 summary 好 summary',
+    modifiedOn: 1740986382961,
+    createdBy: '67BBE095E1621E19CBC5D767',
+    author: 'lcl',
+    modifiedBy: '67BBE095E1621E19CBC5D767',
+    id: '67C5580EE2FD5A1A87B49DB3',
+    title: '家人们好',
+    createdOn: 1740986382961,
+    content: '家人们好 content 家人们好 content 家人们好 content'
+  }
+]
